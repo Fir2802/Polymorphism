fdeac31a66863d44a1f5335faff295a5da3a1e7a
Lakey <lakey.2802@students.amikom.ac.id>
2020-05-08 23:45:01 +0700

    pesan

---
+++ laserjet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB._5
+{
+    class laserjet : printerwindows
+    {
+        public override void show()
+        {
+            Console.WriteLine("laserjet display dimension : 12*12");
+        }
+        public override void print()
+        {
+            Console.WriteLine("laserjet printer printing");
+        }
+    }
+}
